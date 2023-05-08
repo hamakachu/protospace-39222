ef18672d5dbf1864782de7df44124adff5595887
hamakachu <mentaiko0202@yahoo.co.jp>
2023-05-08 10:44:14 +0900

    投稿に必要な情報を入力すると、プロトタイプが編集できる

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -25,7 +25,7 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    if @prototype.id != current_user.id
+    if @prototype.user_id != current_user.id
       redirect_to root_path
     end
   end
